2b2ffc64ab696a55ad4b1af071cf2cec3a09d5c3
Oleg Lavrovsky <loleg@users.noreply.github.com>
2024-11-01 13:47:46 +0100

    Zoom button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,11 +2,11 @@
   <div class="section-footer">
 
     <div container v-if="event.description" class="event-details">
-      <markdown class="description col" 
-         :source="event.description" 
+      <markdown class="description col"
+         :source="event.description"
          :html="true" />
     </div>
-    
+
   </div>
 </template>
 
@@ -47,9 +47,10 @@ export default {
   padding: 0.5em 1em;
   border: 1px solid rgba(0,0,0,0.3);
   border-radius: 6px;
-  background: #fafafa;
-  box-shadow: 5px 5px 0px rgba(0,0,0,0.3);
+  background: rgba(255, 230, 255, 0.6);
+  box-shadow: 5px 5px 0px rgba(0,0,0,0.2);
   font-size: 90%;
+  line-height: 140%;
   text-align: left;
   overflow: hidden;
 }
